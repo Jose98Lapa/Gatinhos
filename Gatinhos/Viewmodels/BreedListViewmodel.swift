6c310c73fb24c0bc3b0a654e95f571d3e286aefb
José Lapa <josemmlapa@gmail.com>
2024-08-17 17:22:47 +0100

    Created simple view model for all list views( in this case both list views apply the same general logic)

--- Gatinhos/Viewmodels/BreedListViewmodel.swift
+++ Gatinhos/Viewmodels/BreedListViewmodel.swift
@@ -10,8 +10,28 @@ import Foundation
 class BreedListViewmodel: ObservableObject {
     
     private var breedRepository: BreedRepository
+    @Published private(set) var breeds: [Breed] = []
+    static private let BreedsPerPage = 20
+    private var page = 0
     
     init(breedRepository: BreedRepository) {
         self.breedRepository = breedRepository
+    }
+    
+    func fetchBreedBy(id: String) -> Breed? {
+        return breedRepository.fetchBreedBy(id: id)
+    }
+    
+    func fetchBreeds(page: Int?, breedsPerPage: Int?) {
+        breeds.append(contentsOf: breedRepository.fetchBreeds(page: page, breedsPerPage: BreedListViewmodel.BreedsPerPage))
+        self.page += 1
+    }
+    
+    func toggleFavouriteForBreed(withId id: String) {
+        return breedRepository.toggleFavouriteForBreed(withId: id)
+    }
+    
+    func getFavourites() -> [Breed] {
+        return breedRepository.getFavourites()
     }
 }
