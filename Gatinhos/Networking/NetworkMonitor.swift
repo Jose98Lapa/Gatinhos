dcb0237f54e4c35133f274fa56d9f7c696d0759c
José Lapa <josemmlapa@gmail.com>
2024-08-17 14:42:04 +0100

    Added network status monitoring class

---
+++ Gatinhos/Networking/NetworkMonitor.swift
@@ -0,0 +1,30 @@
+//
+//  ConnectionHelper.swift
+//  Gatinhos
+//
+//  Created by José Miguel Lapa on 17/08/2024.
+//
+
+
+
+import Foundation
+import Network
+
+class ConnectionHelper: ObservableObject {
+    
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue.global(qos: .background)
+    @Published var isNetworkAccessible: Bool = false
+    
+    init() {
+        setupPathHandler()
+    }
+    
+    func setupPathHandler() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            self?.isNetworkAccessible = path.status == .satisfied
+        }
+        
+        monitor.start(queue: queue)
+    }
+}
