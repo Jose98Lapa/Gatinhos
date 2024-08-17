b9e3e2c2beb78f4a0c65f15f72cc5bd68867bfcf
José Lapa <josemmlapa@gmail.com>
2024-08-17 23:39:48 +0100

    Created thumbnail view for cat breeds

--- Gatinhos/Views/ThumbnailBreedView.swift
+++ Gatinhos/Views/ThumbnailBreedView.swift
@@ -1,21 +1,36 @@
-ZStack {
-                            
-                            Image("cat")
-                                .resizable()
-                                .scaledToFit()
-                                .cornerRadius(6)
-                                .overlay {
-                                    
-                                    ZStack {
-                                        LinearGradient(colors: [.black, .clear], startPoint: .bottom, endPoint: .center)
-                                        
-                                        Text("Gato Lindo")
-                                            .padding(8)
-                                            .lineLimit(1, reservesSpace: false)
-                                            .foregroundStyle(.white)
-                                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
-                                    }
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                                }
-                        }
-                        .clipShape(RoundedRectangle(cornerRadius: 6))
+//
+//  ThumbnailBreedView.swift
+//  Gatinhos
+//
+//  Created by José Miguel Lapa on 17/08/2024.
+//
+
+import SwiftUI
+
+struct ThumbnailBreedView: View {
+    
+    var body: some View {
+        
+        Image("cat")
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(6)
+            .overlay {
+                ZStack {
+                    LinearGradient(colors: [.black, .clear], startPoint: .bottom, endPoint: .center)
+                    
+                    Text("Gato Lindo")
+                        .padding(8)
+                        .lineLimit(1, reservesSpace: false)
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 6))
+    }
+}
+
+#Preview {
+    ThumbnailBreedView()
+}
